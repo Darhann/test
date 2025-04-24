4b8d10832a4b525fe3d2f95678df32316a0784e9
Darkhan <darhan.bb@gmail.com>
2025-04-24 11:50:57 +0500

    Add Variables

---
+++ src/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String [] args){
+        int myInt = 557;
+        short myShort = 3266;
+        long myLong = 26542558;
+
+        double myDouble = 235.35;
+        float myFloat = 2652.4f;
+
+        char c = 'a';
+        boolean b = true;
+
+        byte myByte = 100; //Комментарии
+        System.out.println(myInt);
+    }
+}
