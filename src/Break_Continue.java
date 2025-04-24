9b80bff9fded9189bdd9341fa84b69e73e0baf15
Darkhan <darhan.bb@gmail.com>
2025-04-24 13:23:34 +0500

    Add Break_Continue

---
+++ src/Break_Continue.java
@@ -0,0 +1,24 @@
+public class Break_Continue {
+    public static void main(String [] args) {
+        int i = 0;
+        while(true){
+            if(i == 15){
+                break;
+            }
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Мы вышли из цикла");
+
+        System.out.println();
+
+        for(int x =0; x <= 15; x++){
+            if(x % 2 == 0){
+                continue;
+            }
+            System.out.println("Это нечетное число " + x);
+        }
+    }
+}
+
+
